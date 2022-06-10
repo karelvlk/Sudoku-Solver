a35b6e3284ee6303d7adde2e60412a8cadc8d3aa
Karel Vlk <kaja.vlk@gmail.com>
2022-06-10 15:36:42 +0200

    added backtracking algorithm

---
+++ SudokuSolverApp/SudokuSolverApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolverApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Test()
+        {
+            Solver solver = new Solver();
+            /*int[,] board = new int[,]
+            {
+                { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
+            };*/
+
+            int[,] board = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+
+            };
+            Console.WriteLine("IS IT POSSIBLE TO SOLVE");
+            Console.WriteLine(solver.SolveByBacktracking(board));
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i, j]);
+                    Console.Write(" ");
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Test();
+        }
+    }
+}
---
+++ SudokuSolverApp/SudokuSolverApp/Solver.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolverApp
+{
+    class Solver
+    {
+        private bool CanBePlacedInHorizontalLine(int[,] board, int[] position, int number)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[position[0], i] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool CanBePlacedInVerticalLine(int[,] board, int[] position, int number)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[i, position[1]] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int GetStartLine(int[] pos)
+        {
+            if (pos[0] < 3)
+            {
+                return 0;
+            }
+            else if (pos[0] < 6)
+            {
+                return 3;
+            }
+            else
+            {
+                return 6;
+            }
+        }
+
+        private int GetStartColumn(int[] pos)
+        {
+            if (pos[1] < 3)
+            {
+                return 0;
+            }
+            else if (pos[1] < 6)
+            {
+                return 3;
+            }
+            else
+            {
+                return 6;
+            }
+        }
+
+        private bool CanBePlacedIn3x3Box(int[,] board, int[] position, int number)
+        {
+            int startLine = GetStartLine(position);
+            int startColumn = GetStartColumn(position);
+
+            for (int line = 0; line < 3; line++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (board[line+startLine, column+startColumn] == number)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool CanTheNumberBePlaced(int[,] board, int[] currentChangedPosition, int num)
+        {
+            if (
+                CanBePlacedInHorizontalLine(board, currentChangedPosition, num) &&
+                CanBePlacedInVerticalLine(board, currentChangedPosition, num) &&
+                CanBePlacedIn3x3Box(board, currentChangedPosition, num)
+            )
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+   
+        public int[] GetEmptyPosition(int[,] board)
+        {
+            int row = -1;
+            int col = -1;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        return new int[] { row, col }; ;
+                    }
+                }
+            }
+
+            return new int[] { row, col };
+        }
+
+        public bool SolveByBacktracking(int[,] board)
+        {
+            int[] emptyPosition = GetEmptyPosition(board);
+            if (emptyPosition[0] == -1 && emptyPosition[1] == -1)
+            {
+                return true;
+            } 
+            else
+            {
+                // Backtrack
+                int row = emptyPosition[0];
+                int col = emptyPosition[1];
+
+                for (int num = 1; num <= 9; num++)
+                {
+                    if (CanTheNumberBePlaced(board, new int[] { row, col }, num))
+                    {
+                        board[row, col] = num;
+                        if (SolveByBacktracking(board))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            // Replace wrong value by no value
+                            board[row, col] = 0;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
